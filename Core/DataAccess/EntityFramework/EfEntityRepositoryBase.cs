5fe9795be578c6d093375d308480d0dadb35c108
okan kaipoglu <kaipoglu267@gmail.com>
2022-08-30 01:58:42 +0300

    EfEntityRepositoryBase class olusturulup,Iproductdal implement edildi

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,84 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            using (TContext db = new TContext())
+            {
+                var addedEntity = db.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                db.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext db = new TContext())
+            {
+                var deleteddEntity = db.Entry(entity);
+                deleteddEntity.State = EntityState.Deleted;
+                db.SaveChanges();
+
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext db = new TContext())
+            {
+                return db.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext db = new TContext())
+            {
+
+                //filter null ise product tı listeleyip getir
+                //degilse sarta gore data geitr 
+
+                //if (filter==null)
+                //{
+                //  return db.products.ToList();
+                //}
+                //else
+                //{
+                //   return db.products.Where(filter).ToList();
+                //}
+
+                return filter == null
+               ? db.Set<TEntity>().ToList()
+               : db.Set<TEntity>().Where(filter).ToList();
+
+
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext db = new TContext())
+            {
+                var updateddEntity = db.Entry(entity);
+                updateddEntity.State = EntityState.Modified;
+                db.SaveChanges();
+
+            }
+        }
+
+
+
+    }
+}
--- DataAccessLayer/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccessLayer/Concrete/EntityFramework/EfProductDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccessLayer.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccessLayer.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -10,72 +11,76 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete.EntityFramework
 {
-    public class EfProductDal : IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Products,NorthwindDbContext>,IProductDal
     {
+        ////////////////////////////
+        #region code refactoring'den onceki hali,sadece IProductDal Hali
+        //public void Add(Products entity)
+        //{
+        //    using (NorthwindDbContext db = new NorthwindDbContext())
+        //    {
+        //        var addedEntity = db.Entry(entity);
+        //        addedEntity.State = EntityState.Added;
+        //        db.SaveChanges();
+        //    }
+        //}
 
-        public void Add(Products entity)
-        {
-            using (NorthwindDbContext db = new NorthwindDbContext())
-            {
-                var addedEntity = db.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                db.SaveChanges();
-            }
-        }
+        //public void Delete(Products entity)
+        //{
+        //    using (NorthwindDbContext db = new NorthwindDbContext())
+        //    {
+        //        var deleteddEntity = db.Entry(entity);
+        //        deleteddEntity.State = EntityState.Deleted;
+        //        db.SaveChanges();
 
-        public void Delete(Products entity)
-        {
-            using (NorthwindDbContext db = new NorthwindDbContext())
-            {
-                var deleteddEntity = db.Entry(entity);
-                deleteddEntity.State = EntityState.Deleted;
-                db.SaveChanges();
+        //    }
+        //}
 
-            }
-        }
+        //public Products Get(Expression<Func<Products, bool>> filter)
+        //{
+        //    using (NorthwindDbContext db = new NorthwindDbContext())
+        //    {
+        //        return db.Set<Products>().SingleOrDefault(filter);
+        //    }
+        //}
 
-        public Products Get(Expression<Func<Products, bool>> filter)
-        {
-            using (NorthwindDbContext db = new NorthwindDbContext())
-            {
-                return db.Set<Products>().SingleOrDefault(filter);
-            }
-        }
+        //public List<Products> GetAll(Expression<Func<Products, bool>> filter = null)
+        //{
+        //    using (NorthwindDbContext db = new NorthwindDbContext())
+        //    {
 
-        public List<Products> GetAll(Expression<Func<Products, bool>> filter = null)
-        {
-            using (NorthwindDbContext db = new NorthwindDbContext())
-            {
+        //        //filter null ise product tı listeleyip getir
+        //        //degilse sarta gore data geitr 
 
-                //filter null ise product tı listeleyip getir
-                //degilse sarta gore data geitr 
+        //        //if (filter==null)
+        //        //{
+        //        //  return db.products.ToList();
+        //        //}
+        //        //else
+        //        //{
+        //        //   return db.products.Where(filter).ToList();
+        //        //}
 
-                //if (filter==null)
-                //{
-                //  return db.products.ToList();
-                //}
-                //else
-                //{
-                //   return db.products.Where(filter).ToList();
-                //}
+        //        return filter == null
+        //       ? db.Set<Products>().ToList()
+        //       : db.Set<Products>().Where(filter).ToList();
 
-                     return filter == null
-                    ? db.Set<Products>().ToList()
-                    : db.Set<Products>().Where(filter).ToList();
-                
 
-            }
-        }
+        //    }
+        //}
 
-        public void Update(Products entity)
-        {
-            using (NorthwindDbContext db = new NorthwindDbContext())
-            {
-                var updateddEntity = db.Entry(entity);
-                updateddEntity.State = EntityState.Modified;
-                db.SaveChanges();
+        //public void Update(Products entity)
+        //{
+        //    using (NorthwindDbContext db = new NorthwindDbContext())
+        //    {
+        //        var updateddEntity = db.Entry(entity);
+        //        updateddEntity.State = EntityState.Modified;
+        //        db.SaveChanges();
 
-            }
-        }
+        //    }
+        //}
+        #endregion 
+        ////////////////////////////
+
     }
 }
